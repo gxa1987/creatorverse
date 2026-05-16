c686039ecc76bc3c323c6da6c62349fd9453cf2f
Govinda Awasthi <gxa1987@mavs.uta.edu>
2026-05-16 13:43:46 -0500

    viewing all content creators

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,8 +1,46 @@
+// Importing necessary tools:
+
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { supabase } from '../client';
+import CreatorCard from '../components/CreatorCard';
+
 function ShowCreators() {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    async function fetchCreators() {
+      const { data, error } = await supabase
+        .from('creators')
+        .select()
+        .order('created_at', { ascending: false });
+
+      if (error) {
+        console.log('Error fetching creators:', error);
+      } else {
+        setCreators(data);
+      }
+    }
+
+    fetchCreators();
+  }, []);
+
   return (
     <div>
-      <h1>All Creators</h1>
-      <p>it will show all creators.</p>
+      <h1>Creatorverse</h1>
+      <p>My favorite content creators.</p>
+
+      <Link to="/add">Add New Creator</Link>
+
+      <div>
+        {creators.length > 0 ? (
+          creators.map((creator) => (
+            <CreatorCard key={creator.id} creator={creator} />
+          ))
+        ) : (
+          <p>No creators yet. Add one!</p>
+        )}
+      </div>
     </div>
   );
 }
