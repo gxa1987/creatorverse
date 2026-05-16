c9dbd075ec2e249cb39445ce12f485de3ee71c2f
Govinda Awasthi <gxa1987@mavs.uta.edu>
2026-05-16 13:22:23 -0500

    creator card component created

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,11 +1,33 @@
-function CreatorCard() {
+// Link tools from react router
+import { Link } from 'react-router-dom';
+
+// creating react component creatorCard
+function CreatorCard({ creator }) {
   return (
-    <div>
-      <h2>Creator Card</h2>
-      <p>Creator Card</p>
+    <div className="creator-card">
+      {creator.imageURL && (
+        <img
+          src={creator.imageURL}
+          alt={creator.name}
+          className="creator-image"
+        />
+      )}
+
+      <h2>{creator.name}</h2>
+
+      <p>{creator.description}</p>
+
+      <a href={creator.url} target="_blank" rel="noreferrer">
+        Visit Channel
+      </a>
+
+      <br />
+
+      <Link to={`/creator/${creator.id}`}>View Details</Link>
+      {' | '}
+      <Link to={`/edit/${creator.id}`}>Edit</Link>
     </div>
   );
 }
-
 
 export default CreatorCard;
